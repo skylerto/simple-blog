e453f0ea56214d3d81d8406ec68a9fa74b83252a
skylerto <skylerclayne@gmail.com>
2015-07-29 12:44:21 -0400

    user post names and working buttons for firefox

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:user_name, :title, :body)
   end
 end
